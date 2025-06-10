5aca3f7b38c74c1ecec0ef2daaa2d681fab48c54
WOOEUNJU <wooeunju7496@gmail.com>
2025-06-10 12:03:48 +0900

    desgin: 1219px 미디어쿼리 작성 중

--- js/sideBanner.js
+++ js/sideBanner.js
@@ -1,10 +1,15 @@
 const sideBannerWrap = document.querySelector(".side-banner-wrap");
 const sideBannerList = sideBannerWrap.querySelectorAll(".side-banner");
+const sideBannerWrapMb = document.querySelector(".side-banner-wrap-mb");
+const sideBannerListMb = sideBannerWrapMb.querySelectorAll(".side-banner");
 const style = {
-  bgColorList: ["#edf4ff", "#def9fe", "#fff6de"],
+ bgColorList: ["#edf4ff", "#def9fe", "#fff6de"],
 };
 // console.log(sideBannerList);
 
 sideBannerList.forEach((sideBanner, index) => {
-  sideBanner.style.backgroundColor = style.bgColorList[index];
+ sideBanner.style.backgroundColor = style.bgColorList[index];
+});
+sideBannerListMb.forEach((sideBannerMb, index) => {
+ sideBannerMb.style.backgroundColor = style.bgColorList[index];
 });
